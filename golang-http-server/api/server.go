fdb14cc659cfd21a2079ba70d3f08744eceba462
NoahSaberhagen <99302102+NoahSaberhagen@users.noreply.github.com>
2025-01-10 11:53:20 -0500

    add api

---
+++ golang-http-server/api/server.go
@@ -0,0 +1,60 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+)
+
+type Item struct {
+	ID   uuid.UUID `json:"id"`
+	Name string    `json:"name"`
+}
+
+type Server struct {
+	// server composes mux Router
+	// --> inherits all methods
+	*mux.Router
+
+	shoppingItems []Item
+}
+
+func NewServer() *Server {
+	s := &Server{
+		Router:        mux.NewRouter(),
+		shoppingItems: []Item{},
+	}
+	return s
+}
+
+// route handlers appear as server methods
+func (s *Server) createShoppingItem() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var i Item
+		if err := json.NewDecoder(r.Body).Decode(&i); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		i.ID = uuid.New()
+		s.shoppingItems = append(s.shoppingItems, i)
+
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(i); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func (s *Server) listShoppingItems() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(s.shoppingItems); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
